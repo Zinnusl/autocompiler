511647a8bedd681d50becadf056f43284109a9c2
Zinnusl <Zaraxx@draig.de>
2017-12-09 20:26:38 +0100

    Make App read grammar file on creation

--- src/App.rb
+++ src/App.rb
@@ -1,8 +1,12 @@
+require './src/Grammar'
+
 class App
 	attr_reader :test_files
 
 	def initialize(args)
 		@test_files = args.select {|arg| arg.match /.+\.cpp/}
+
+		Grammar.parse 'cpp.grammar'
 
 		File.open('build/out.exe', 'w') do |file|
 			file.write "test"
--- test/app_test.rb
+++ test/app_test.rb
@@ -1,6 +1,11 @@
 require './src/App'
 
 describe App do
+	it 'should parse a grammar file on start' do
+		expect(File).to receive(:read).with(/\.grammar/).and_return("`a` -> `b`\n`b` -> `c`\n")
+
+		app = App.new [""]
+	end
 	it 'should create a executable (.exe)' do
 		file = double('file')
 		expect(File).to receive(:open).with("build/out.exe", "w").and_yield(file)
