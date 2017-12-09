c443eb2c2d87f58021cd4662383ed0bfbdd9ffa2
Zinnusl <Zaraxx@draig.de>
2017-12-09 19:59:48 +0100

    Make App create a executable upon creation

--- src/App.rb
+++ src/App.rb
@@ -3,5 +3,9 @@ class App
 
 	def initialize(args)
 		@test_files = args.select {|arg| arg.match /.+\.cpp/}
+
+		File.open('build/out.exe', 'w') do |file|
+			file.write "test"
+		end
 	end
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -1,6 +1,13 @@
 require './src/App'
 
 describe App do
+	it 'should create a executable (.exe)' do
+		file = double('file')
+		expect(File).to receive(:open).with("build/out.exe", "w").and_yield(file)
+		expect(file).to receive(:write).with("test")
+
+		app = App.new [""]
+	end
 	it 'should accept a list of cpp source files' do
 		args = [ "a.cpp", "b.cpp", "c.cpp", "ruby.rb"]
 
